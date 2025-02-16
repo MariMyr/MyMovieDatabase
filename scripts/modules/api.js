bfb6e834125377b8a8db54858ecec681ed0ac5d4
Mari Myrmoen <mari.myr@gmail.com>
2025-02-16 20:01:17 +0100

    Moviecard function plu styling

--- scripts/modules/api.js
+++ scripts/modules/api.js
@@ -13,4 +13,28 @@ export async function fetchTopMovies() {
     console.error("Error fetching movies: ", error);
     }
 }
+export async function movieCards() {
+    // Vänta på att API:et ska hämta filmen innan renderingen
+    await fetchTopMovies();
 
+    const container = document.querySelector('#cardContainer');
+    oData.topMovieList.forEach(movie => {
+        // skapa ett kort
+        const cardRef = document.createElement('article');
+        cardRef.className = 'movie-card'; // Lägg gärna till en klass för styling
+
+        // Lägg till titel
+        const titleRef = document.createElement('h3');
+        titleRef.textContent = movie.Title;
+        cardRef.appendChild(titleRef);
+
+        // Lägg till Poster
+        const posterRef = document.createElement('img');
+        posterRef.src = movie.Poster;
+        posterRef.alt = `${movie.title} poster`;
+        cardRef.appendChild(posterRef);
+
+        // Lägg till kortet i container
+        container.appendChild(cardRef);
+    });
+}
