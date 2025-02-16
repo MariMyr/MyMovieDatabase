1f7fb0d4bde0e16383eb2747f607a8556c19ebf0
Mari Myrmoen <mari.myr@gmail.com>
2025-02-16 20:17:12 +0100

    added new js.file - movieCard.js

--- scripts/components/movieCard.js
+++ scripts/components/movieCard.js
@@ -1,27 +1,23 @@
-// import { fetchTopMovies, oData } from "./modules/api.js";
+export function movieCards(movieList) {
 
-// export async function movieCards() {
-//     // Vänta på att API:et ska hämta filmen innan renderingen
-//     await fetchTopMovies();
+    const container = document.querySelector('#cardContainer');
+    movieList.forEach(movie => {
+        // skapa ett kort
+        const cardRef = document.createElement('article');
+        cardRef.className = 'movie-card'; // Lägg gärna till en klass för styling
 
-//     const container = document.querySelector('#cardContainer');
-//     oData.topMovieList.forEach(movie => {
-//         // skapa ett kort
-//         const cardRef = document.createElement('article');
-//         cardRef.className = 'movie-card'; // Lägg gärna till en klass för styling
+        // Lägg till titel
+        const titleRef = document.createElement('h3');
+        titleRef.textContent = movie.Title;
+        cardRef.appendChild(titleRef);
 
-//         // Lägg till titel
-//         const titleRef = document.createElement('h3');
-//         titleRef.textContent = movie.title;
-//         cardRef.appendChild(titleRef);
+        // Lägg till Poster
+        const posterRef = document.createElement('img');
+        posterRef.src = movie.Poster;
+        posterRef.alt = `${movie.Title} poster`;
+        cardRef.appendChild(posterRef);
 
-//         // Lägg till Poster
-//         const posterRef = document.createElement('img');
-//         posterRef.src = movie.poster;
-//         poster.alt = `${movie.title} poster`;
-//         cardRef.appendChild(posterRef);
-
-//         // Lägg till kortet i container
-//         container.appendChild(cardRef);
-//     });
-// }
+        // Lägg till kortet i container
+        container.appendChild(cardRef);
+    });
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,30 @@
-import { fetchTopMovies, oData, movieCards } from "./modules/api.js";
+import { fetchTopMovies, oData } from "./modules/api.js";
 import { renderTrailers } from "./modules/caroussel.js";
-// import { movieCards } from "./components/movieCard.js";
+import { movieCards } from "./components/movieCard.js";
 
-if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
+if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
 
-} else if(window.location.pathname === '/favorites.html') {
+} else if (window.location.pathname === '/favorites.html') {
     console.log('favorites.html');
 
-} else if(window.location.pathname === '/movie.html') {
+} else if (window.location.pathname === '/movie.html') {
     console.log('movie.html');
 
-} else if(window.location.pathname === '/search.html') {
+} else if (window.location.pathname === '/search.html') {
     console.log('search.html');
 
 }
 
-// async function setupPage() {
-//     try {
-//         // Hämta topplistan av filmer
-//         await fetchTopMovies();
-
-//         // Om API-anropet inte lyckas, ge ett felmeddelande
-//         if (oData.topMovieList.length === 0) {
-//             throw new Error('Ingen filmdata hittades!');
-//         }
-
-//         // Välj slumpmässiga filmer för trailers
-//         const randomMovies = oData.topMovieList.sort(() => 0.5 - Math.random()).slice(0, 5);
-//         randomMovies.forEach((movie, i) => renderTrailers(movie, i + 1));
-
-//         // Skapa movieCards
-//         // movieCards();
-//     } catch (error) {
-//         console.error("Fel vid hämtning av filmer: ", error);
-//     }
-// }
-
-// setupPage();
-
 async function setupPage() {
-    await fetchTopMovies();
-    const randomMovies = oData.topMovieList.sort(() => 0.5 - Math.random()).slice(0, 5);
-    randomMovies.forEach((movie, i) => renderTrailers(movie, i + 1));
-    movieCards();
+    await fetchTopMovies(); // Vänta på att filmerna hämtas
+
+    if (oData.topMovieList.length > 0) {
+        movieCards(oData.topMovieList); // Skickar in filmlistan till movieCards()
+        const randomMovies = oData.topMovieList.sort(() => 0.5 - Math.random()).slice(0, 5);
+        randomMovies.forEach((movie, i) => renderTrailers(movie, i + 1));
+    } else {
+        console.error("Ingen filmlista hittades.");
+    }
 }
-
 setupPage();
-
