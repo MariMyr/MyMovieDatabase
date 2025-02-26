2359a9bd2191017eea79d1771e82d726406ffb2d
Mari Myrmoen <mari.myr@gmail.com>
2025-02-26 15:15:35 +0100

    Created autocomplete function for search input

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -1,3 +1,5 @@
+import { fetchSearchedMovies } from "../modules/api.js";
+
 export function setupSearchForm() {
     const formRef = document.querySelector('#searchForm');
     const inputRef = document.querySelector('#searchInput');
@@ -6,4 +8,31 @@ export function setupSearchForm() {
         event.preventDefault();
         window.location.href = `search.html?movie=${inputRef.value}`;
     })
+    autocompleteList(inputRef);
+
+}
+
+function autocompleteList(inputRef) {
+    const datalistRef = document.querySelector('#searchSuggestions');
+
+    inputRef.addEventListener('input', async () => {
+        const query = inputRef.value.trim().toLowerCase();
+
+        if (query.length < 3) {
+            datalistRef.innerHTML = '';
+            return;
+        }
+        try {
+            const movies = await fetchSearchedMovies(query);
+            datalistRef.innerHTML = '';
+            movies.forEach(movie => {
+                const option = document.createElement('option');
+                option.value = movie.Title;
+                datalistRef.appendChild(option);
+            });
+        } catch (error) {
+            console.log('Error fetching autocomplete data:', error);
+        }
+    });
+    
 }
